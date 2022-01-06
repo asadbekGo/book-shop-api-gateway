ae984f17b01dcf6ffea7832f505569440f18f998
JasurbekUz <jasurbekedu@gmail.com>
2022-01-06 13:02:40 +0500

    proto files added and generated

---
+++ scripts/gen_proto.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+CURRENT_DIR=$(pwd)
+
+
+protoc -I /usr/local/include \
+       -I $GOPATH/src/github.com/gogo/protobuf/gogoproto \
+       -I $CURRENT_DIR/protos/catalog_service/ \
+        --gofast_out=plugins=grpc:$CURRENT_DIR/genproto/catalog_service/ \
+        $CURRENT_DIR/protos/catalog_service/*.proto;
+
+protoc -I /usr/local/include \
+       -I $GOPATH/src/github.com/gogo/protobuf/gogoproto \
+       -I $CURRENT_DIR/protos/order_service/ \
+        --gofast_out=plugins=grpc:$CURRENT_DIR/genproto/order_service/ \
+        $CURRENT_DIR/protos/order_service/*.proto;        
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i "" -e "s/,omitempty//g" $CURRENT_DIR/genproto/*/*.go
+  else
+    sed -i -e "s/,omitempty//g" $CURRENT_DIR/genproto/*/*.go
+fi
