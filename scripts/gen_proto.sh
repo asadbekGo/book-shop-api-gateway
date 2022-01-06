b0e2eec29b8e9b963083292a354705308a751c20
JasurbekUz <jasurbekedu@gmail.com>
2022-01-06 23:30:29 +0500

    bash file updated

--- scripts/gen_proto.sh
+++ scripts/gen_proto.sh
@@ -2,7 +2,6 @@
 
 CURRENT_DIR=$(pwd)
 
-
 protoc -I /usr/local/include \
        -I $GOPATH/src/github.com/gogo/protobuf/gogoproto \
        -I $CURRENT_DIR/protos/catalog_service/ \
@@ -16,7 +15,13 @@ protoc -I /usr/local/include \
         $CURRENT_DIR/protos/order_service/*.proto;        
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    sed -i "" -e "s/,omitempty//g" $CURRENT_DIR/genproto/*/*.go
+    sed -i "" -e "s/,omitempty//g" $CURRENT_DIR/genproto/catalog_service/*.go
   else
-    sed -i -e "s/,omitempty//g" $CURRENT_DIR/genproto/*/*.go
+    sed -i -e "s/,omitempty//g" $CURRENT_DIR/genproto/catalog_service/*.go
+fi
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i "" -e "s/,omitempty//g" $CURRENT_DIR/genproto/order_service/*.go
+  else
+    sed -i -e "s/,omitempty//g" $CURRENT_DIR/genproto/order_service/*.go
 fi
