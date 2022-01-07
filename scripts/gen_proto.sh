c51b70e54a130808a24a4ec8928c5b05da3466fa
Jasurbek Shamsitdinov <87220133+JasurbekUz@users.noreply.github.com>
2022-01-07 23:18:16 +0500

    -
    -

--- scripts/gen_proto.sh
+++ scripts/gen_proto.sh
@@ -2,15 +2,26 @@
 
 CURRENT_DIR=$(pwd)
 
+protoc -I /usr/local/include \
+       -I $GOPATH/src/github.com/gogo/protobuf/gogoproto \
+       -I $CURRENT_DIR/protos/catalog_service/ \
+        --gofast_out=plugins=grpc:$CURRENT_DIR/genproto/catalog_service/ \
+        $CURRENT_DIR/protos/catalog_service/*.proto;
 
 protoc -I /usr/local/include \
        -I $GOPATH/src/github.com/gogo/protobuf/gogoproto \
        -I $CURRENT_DIR/protos/order_service/ \
         --gofast_out=plugins=grpc:$CURRENT_DIR/genproto/order_service/ \
-        $CURRENT_DIR/protos/order_service/*.proto;
+        $CURRENT_DIR/protos/order_service/*.proto;        
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    sed -i "" -e "s/,omitempty//g" $CURRENT_DIR/genproto/*.go
+    sed -i "" -e "s/,omitempty//g" $CURRENT_DIR/genproto/catalog_service/*.go
   else
-    sed -i -e "s/,omitempty//g" $CURRENT_DIR/genproto/*.go
+    sed -i -e "s/,omitempty//g" $CURRENT_DIR/genproto/catalog_service/*.go
+fi
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i "" -e "s/,omitempty//g" $CURRENT_DIR/genproto/order_service/*.go
+  else
+    sed -i -e "s/,omitempty//g" $CURRENT_DIR/genproto/order_service/*.go
 fi
